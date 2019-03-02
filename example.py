40f5eb1b65588b4ed2d0475493fc48376da3ea1b
mdl-admin <mdl-admin@PSYC-887260.austin.utexas.edu>
2019-03-01 18:57:11 -0600

    Signed-off-by: mdl-admin <mdl-admin@PSYC-887260.austin.utexas.edu>

--- example.py
+++ example.py
@@ -20,41 +20,41 @@ sys.path.insert(0, '.\lib')
 # ----------------pre-task
 # initalize
 """
-    Parameters
-    ----------
-    libraries : :class:`bool`
-        Should the code check if required libraries are available.
-    sample_rate : :class:`int`
-        Sampling rate (250, 500, 1000, or 2000).
-    c_num : :class:`int`
-        Calibration type. Default is 13-point.
-    paval : :class:`int`
-        Set calibraiton pacing
-    pupil_size : :class:`int`
-        Pupil Size (area, perimeter).
-    ip : :class:`int`
-        IP Address to connect to Eyelink computer.
-    s_port : :class:`int`
-        Port address to send data.
-    r_port : :class:`int`
-        Port address to recieve data.
-    saccade_velocity_threshold : :class:`int`
-        Saccade Velocity Threshold. Default is 35.
-    saccade_acceleration_threshold : :class:`int`
-        Saccade Acceleration Threshold. Default is 9500.
-    recording_parse_type : :class:`str`
-        Options are either 'GAZE','HREF'.
-    enable_search_limits : :class:`bool`
-        s
-    track_search_limits : :class:`bool`
-        s
-    autothreshold_click : :class:`bool`
-        s
-    autothreshold_repeat : :class:`bool`
-        a
-    enable_camera_position_detect : :class:`bool`
-        a
-    """
+Parameters
+----------
+libraries : :class:`bool`
+    Should the code check if required libraries are available.
+sample_rate : :class:`int`
+    Sampling rate (250, 500, 1000, or 2000).
+c_num : :class:`int`
+    Calibration type. Default is 13-point.
+paval : :class:`int`
+    Set calibraiton pacing
+pupil_size : :class:`int`
+    Pupil Size (area, perimeter).
+ip : :class:`int`
+    IP Address to connect to Eyelink computer.
+s_port : :class:`int`
+    Port address to send data.
+r_port : :class:`int`
+    Port address to recieve data.
+saccade_velocity_threshold : :class:`int`
+    Saccade Velocity Threshold. Default is 35.
+saccade_acceleration_threshold : :class:`int`
+    Saccade Acceleration Threshold. Default is 9500.
+recording_parse_type : :class:`str`
+    Options are either 'GAZE','HREF'.
+enable_search_limits : :class:`bool`
+    s
+track_search_limits : :class:`bool`
+    s
+autothreshold_click : :class:`bool`
+    s
+autothreshold_repeat : :class:`bool`
+    a
+enable_camera_position_detect : :class:`bool`
+    a
+"""
 subject = 1
 eyetracking = mdl.eyetracking(libraries=False, subject=subject, sample_rate=500, c_num=13, paval=1000,
                               pupil_size="area", ip="127.0.0.1", s_port=4444, r_port=5555,
@@ -71,11 +71,11 @@ window = visual.Window(
 
 # set dominant eye
 """
-    Parameters
-    ----------
-    eye : :obj:`str`
-        Dominant eye, either left or right. This will be used for outputting Eyelink gaze samples.
-    """
+Parameters
+----------
+eye : :obj:`str`
+    Dominant eye, either left or right. This will be used for outputting Eyelink gaze samples.
+"""
 dominant_eye = 'left'
 eye_used = eyetracking.set_eye_used(eye=dominant_eye)
 
@@ -83,77 +83,77 @@ eye_used = eyetracking.set_eye_used(eye=dominant_eye)
 # ----------------calibration
 # calibration
 """
-    Parameters
-    ----------
-    window : :class:`psychopy.visual.window.Window`
-        PsychoPy window instance.
-    """
+Parameters
+----------
+window : :class:`psychopy.visual.window.Window`
+    PsychoPy window instance.
+"""
 eyetracking.calibration(window=window)
 #----------------debugging (optional)
 """
-    Parameters
-    ----------
-    color : :class:`str`
-        Color to use (black, red, green, orange, purple, blue, grey).
-    msg : :class:`str`
-        Message to be color printed.
-    """
+Parameters
+----------
+color : :class:`str`
+    Color to use (black, red, green, orange, purple, blue, grey).
+msg : :class:`str`
+    Message to be color printed.
+"""
 eyetracking.console(c="green", msg="eyetracking.calibration() started")
 
 # %%
 # ----------------within trial
 # start recording
 """
-    Parameters
-    ----------
-    trial : :obj:`int`
-        Trial Number.
-    block : :obj:`int`
-         Block Number.
-    variables : :obj:`dict`
-         Dict of variables to send to eyelink (variable name, value).
-    """
+Parameters
+----------
+trial : :obj:`int`
+    Trial Number.
+block : :obj:`int`
+     Block Number.
+variables : :obj:`dict`
+     Dict of variables to send to eyelink (variable name, value).
+"""
 eyetracking.start_recording(trial=1, block=1)
 
 # %%
 # Collects new gaze coordinates from Eyelink (only if needed in experiment)
 """
-    Parameters
-    ----------
-    eye_used : :obj:`str`
-        Checks if eye used is available.
-    """
+Parameters
+----------
+eye_used : :obj:`str`
+    Checks if eye used is available.
+"""
 gxy = eyetracking.sample(eye_used=eye_used)
 
 # %%
 # Send messages to eyelink.
 """
-    Parameters
-    ----------
-    msg : :obj:`str`
-        Message to be recieved by eyelink.
-    """
+Parameters
+----------
+msg : :obj:`str`
+    Message to be recieved by eyelink.
+"""
 msg = "stimulus onset"
 eyetracking.send_message(msg=msg)
 
 # %%
 # Stops recording of eyelink. Also allows transmission of trial-level variables to Eyelink.
 """
-    Stops recording of eyelink. Also allows transmission of trial-level variables to Eyelink.
+Stops recording of eyelink. Also allows transmission of trial-level variables to Eyelink.
 
-    Parameters
-    ----------
-    image : :obj:`str`
-        Image to be displayed in Eyelink software.
-    trial : :obj:`int`
-        Trial Number.
-    block : :obj:`int`
-        Block Number.
-    block : :obj:`int`
-        Accuracy (1=correct, 0=incorrect).
-    variables : :obj:`dict`
-        Dict of variables to send to eyelink (variable name, value).
-    """
+Parameters
+----------
+image : :obj:`str`
+    Image to be displayed in Eyelink software.
+trial : :obj:`int`
+    Trial Number.
+block : :obj:`int`
+    Block Number.
+block : :obj:`int`
+    Accuracy (1=correct, 0=incorrect).
+variables : :obj:`dict`
+    Dictionary of variables to send to eyelink (variable name, value).
+"""
 variables = dict(stimulus='img.jpg',
                  trial_type='fixation',
                  race="white")
@@ -162,9 +162,9 @@ eyetracking.stop_recording(trial=1, block=1, variables=variables)
 # %%
 # Finish recording of eyelink.
 """
-    Parameters
-    ----------
-    subject : :obj:`int`
-        subject number.
-    """
+Parameters
+----------
+subject : :obj:`int`
+    subject number.
+"""
 eyetracking.finish_recording()
