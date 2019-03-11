4298de80b2fc8112edba51318510dd7bd17ce4c0
Semeon Risom <semeon.risom@gmail.com>
2019-03-11 11:23:23 -0500

    Update

---
+++ mdl/eyelink/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+
+__name__ = 'mdl-eyelink'
+__distname__ = 'mdl-eyelink'
+__version__ = '2019.05.01'
+__license__ = 'GNU GPLv3 (or more recent equivalent)'
+__author__ = 'Semeon Risom'
+__author_email__ = 'semeon.risom@gmail.com'
+__maintainer_email__ = 'semeon.risom@gmail.com'
+__url__ = 'https://semeon.io/d/mdl-eyelink/'
+__download_url__ = 'https://github.com/risoms/mdl-eyelink/'
+__git_sha__ = 'n/a'
+__build_platform__ = 'n/a'
+
+
+from . import pylink
+from .calibration import calibration
+from .eyetracking import eyetracking
+
