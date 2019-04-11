98f845d0cbba1a70d60873c44a012f2764754ac9
Semeon Risom <semeon.risom@gmail.com>
2019-04-11 13:55:20 -0500

    Update

--- mdl/docs/source/examples/eyetracking/demo/demo.py
+++ mdl/docs/source/examples/eyetracking/demo/demo.py
@@ -1,39 +1,40 @@
 #%% [markdown]
-#  <title>Example setup for Eyelink 1000 Plus, using PsychoPy 3.0.</title>
+# .. title:: Example setup for Eyelink 1000 Plus, using PsychoPy 3.0
 #  <h5>Example setup for Eyelink 1000 Plus, using PsychoPy 3.0.</h5>
 # %% [markdown]
-# <a>Created on Wed Feb 13 15:37:43 2019</a><br>
-# <a>@author: Semeon Risom</a><br>
-# <a>@email: semeon.risom@gmail.com</a><br>
-# <a>Sample code to run SR Research Eyelink eyetracking system. Code is optimized for the Eyelink 1000</a><br>
-# <a>Plus (5.0), but should be compatiable with earlier systems.</a><br>
+# <div class='info'><p>Created on Wed Feb 13 15:37:43 2019</p>
+# <p>@author: Semeon Risom</p>
+# <p>@email: semeon.risom@gmail.com</p>
+# </div>
+# Sample code to run SR Research Eyelink eyetracking system. Code is optimized for the Eyelink 1000
+# Plus (5.0), but should be compatiable with earlier systems.
 # %% [markdown]
 # <ul class="list-container">
 #     <li>
 #         <div class="title">The sequence of operations for implementing the trial is:</div>
 #         <ol class="list">
-#             <li>[Import the mdl package](:ref:`demo.ipynb#A-Sub-Section`).</li>
-#             <li>[Initialize the `mdl.eyetracking()` package](demo.html#import).</li>
-#             <li>[Connect to the Eyelink Host](demo#import).</li>
-#             <li>[Set the dominamt eye](demo#import).</li>
-#             <li>[Start calibration](demo#import).</li>
-#             <li>[Start recording](demo#import).</li>
-#             <li>[Stop recording](demo.ipynb#import).</li>
-#             <li>[Finish recording](example.ipynb#import).</li>
+#             <li>[Import packages](demo.html#import).</li>
+#             <li>[Initialize the `mdl.eyetracking()` package](demo.html#init).</li>
+#             <li>[Connect to the Eyelink Host](demo.html#connect).</li>
+#             <li>[Set the dominamt eye](demo.html#eye).</li>
+#             <li>[Start calibration](demo.html#calibration).</li>
+#             <li>[Start recording](demo.html#start).</li>
+#             <li>[Stop recording](demo.html#stop).</li>
+#             <li>[Finish recording](demo.html#finish).</li>
 #         </ol>
 #     </li>
 #     <li>
 #         <div class="title">Optional commands include:</div>
 #         <ol>
-#             <li>Drift correction.</li>
-#             <li>Initiate gaze contigent event.</li>
-#             <li>Collect real-time gaze coordinates from Eyelink.</li>
-#             <li>Send messages to Eyelink.</li>
+#             <li>[Drift correction](demo.html#drift).</li>
+#             <li>[Initiate gaze contigent event](demo.html#gc).</li>
+#             <li>[Collect real-time gaze coordinates from Eyelink](demo.html#sample).</li>
+#             <li>[Send messages to Eyelink](demo.html#message).</li>
 #         </ol>
 #     </li>
 # </ul>
 # %% [markdown]
-# <h5>Import packages.</h5><br>
+# <h5 id='import'>Import packages.</h5><br>
 # %%
 import os, sys, time
 sys.path.append(os.path.abspath(os.getcwd() + '../../../../../../..'))
@@ -51,24 +52,24 @@ routineTimer = core.CountdownTimer()
 window = visual.Window(size=[1920, 1080], fullscr=False, allowGUI=True, units='pix', winType='pyglet', color=[110,110,110], colorSpace='rgb255')
 window.flip()
 # %% [markdown]
-# <h5>Initialize the mdl.eyetracking() package.</h5><br>
-# Note: Before initializing, make sure code is placed after PsychoPy window instance has been created in the experiment file. 
+# <h5 id='init'>Initialize the `mdl.eyetracking()`<mdl.eyetracking()>_ package.</h5><br>
+# .. note:: Before initializing, make sure code is placed after PsychoPy window instance has been created in the experiment file. 
 # This window will be used in the calibration function.
 # %%
 # Initialize mdl.eyetracking()
 eyetracking = mdl.eyetracking.run(window=window, libraries=True, subject=subject, timer=routineTimer, demo=True)
 # %% [markdown]
-# <h5>Connect to the Eyelink Host.</h5><br>
+# <h5 id='connect'>Connect to the Eyelink Host.</h5><br>
 # This controls the parameters to be used when running the eyetracker.
 # %%
 param = eyetracking.connect(calibration_type=13)
 # %% [markdown]
-# <h5>Set the dominamt eye.</h5><br>
+# <h5 id='eye'>Set the dominamt eye.</h5><br>
 # This step is required for recieving gaze coordinates from Eyelink->PsychoPy.
 # %%
 eye_used = eyetracking.set_eye_used(eye=dominant_eye)
 # %% [markdown]
-# <h5>Start calibration.</h5><br>
+# <h5 id='calibration'>Start calibration.</h5><br>
 # %%
 # start calibration
 calibration = eyetracking.calibration()
@@ -77,19 +78,19 @@ calibration = eyetracking.calibration()
 # The Calibration, Validation, 'task-start' events are controlled by the keyboard.
 # Calibration ("c"), Validation ("v"), task-start ("o") respectively.
 # %% [markdown]
-# <h5>(Optional) Print message to console/terminal.</h5><br>
+# <h5 id='print'>(Optional) Print message to console/terminal.</h5><br>
 # Allows printing color coded messages to console/terminal/cmd. This may be useful for debugging issues.
 # %%
 eyetracking.console(c="blue", msg="eyetracking.calibration() started")
 # %% [markdown]
-# <h5>(Optional) Drift correction.</h5><br>
+# <h5 id='drift'>(Optional) Drift correction.</h5><br>
 # This can be done at any point after calibration, including before and after 
 # [eyetracking.start_recording()](eyetracking.rst#mdl.eyetracking.eyetracking.start_recording) has started.
 # %%
 drift = eyetracking.drift_correction()
 # %% [markdown]
-# <h5>Start recording.</h5><br>
-# Note: This should be run at the start of the trial. Also, there is an intentional delay of 150 msec to 
+# <h5 id='start'>Start recording.</h5><br>
+# .. note:: This should be run at the start of the trial. Also, there is an intentional delay of 150 msec to 
 # allow the Eyelink to buffer gaze samples that will show up in your data.
 # %%
 # Create stimulus (demonstration purposes only).
@@ -106,7 +107,7 @@ window.flip()
 # start recording
 eyetracking.start_recording(trial=1, block=1)
 # %% [markdown]
-# <h5>(Optional) Initiate gaze contigent event.</h5><br>
+# <h5 id='gc'>(Optional) Initiate gaze contigent event.</h5><br>
 # This is used for realtime data collection from Eyelink->PsychoPy.
 # %%
 # In the example, a participant is qto look at the bounding cross for a duration
@@ -115,13 +116,13 @@ eyetracking.start_recording(trial=1, block=1)
 bound = dict(left=448, top=156, right=1472, bottom=924)
 eyetracking.gc(bound=bound, t_min=5000, t_max=20000)
 # %% [markdown]
-# <h5>(Optional) Collect real-time gaze coordinates from Eyelink.</h5><br>
-# Note: Samples need to be collected at an interval of 1000/(sampling rate) msec to prevent oversampling.
+# <h5 id='sample'>(Optional) Collect real-time gaze coordinates from Eyelink.</h5><br>
+# .. note:: Samples need to be collected at an interval of 1000/(sampling rate) msec to prevent oversampling.
 # %%
 gxy, ps, sample = eyetracking.sample() # get gaze coordinates, pupil size, and sample
 # %% [markdown]
-# <h5>(Optional) Example use of `eyetracking.sample()`.</h5><br>
-# Note: Samples need to be collected at an interval of 1000/(sampling rate) msec to prevent oversampling.
+# <h5>Example use of `eyetracking.sample()`<eyetracking.sample()>_.</h5><br>
+# .. note:: Samples need to be collected at an interval of 1000/(sampling rate) msec to prevent oversampling.
 # %%
 # In our example, the sampling rate of our device (Eyelink 1000 Plus) is 500Hz.
 s1 = 0 # set current time to 0
@@ -142,25 +143,26 @@ while True:
     #break `while` statement if list of gaze coordiantes >= 20 (not required; demonstration purposes only)
     if len(lgxy) >= 200: break
 # %% [markdown]
-# <h5>(Optional) Send messages to Eyelink.</h5><br>
+# <h5 id='message'>(Optional) Send messages to Eyelink.</h5><br>
 # This allows post-hoc processing of event markers (i.e. "stimulus onset").
 # %%
 # Sending message "stimulus onset".
 eyetracking.send_message(msg="stimulus onset")
 # %% [markdown]
-# <h5>Stop recording.</h5><br>
-# Also (optional) provides trial-level variables to Eyelink.
-# Note: Variables sent are optional. If they being included, they must be in `python dict` format.
+# <h5 id='stop'>Stop recording.</h5><br>
+# Also (optional) provides trial-level variables to Eyelink.  
+# %% [markdown]
+# .. note:: Variables sent are optional. If they being included, they must be in `python dict` format.
 # %%
 # Prepare variables to be sent to Eyelink
 variables = dict(stimulus=filename, trial_type='encoding', race="black")
 # Stop recording
 eyetracking.stop_recording(trial=1, block=1, variables=variables)
 # %% [markdown]
-# <h5>Finish recording.</h5><br>
+# <h5 id='finish'>Finish recording.</h5><br>
 # %%
 eyetracking.finish_recording()
 # %% [markdown]
 # <h5>Close PsychoPy.</h5><br>
 # %%
- window.close()
+window.close()
